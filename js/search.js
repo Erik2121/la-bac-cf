40b1253ed8f7b9572011065d16ca441de4cad3ed
Erik2121 <62885353+Erik2121@users.noreply.github.com>
2021-12-09 15:19:17 +0200

    Add files via upload

--- js/search.js
+++ js/search.js
@@ -3,182 +3,182 @@ const date=[
     {
         numeAutor:"Camil Petre",
         opera:"Particularitatile personajului principal-Ultima nopate de dragoste Intaia nopate de razboi",
-        href:""
+        href:"autori/eseuri/camil-petrecu/eseu-structurat-ultima-nopate-de-dragoste-intaia-nopate-de-razboi-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"Camil Petre",
         opera:"Relatia dintre 2 personaje-Ultima nopate de dragoste Intaia nopate de razboi",
-        href:""
+        href:"autori/eseuri/camil-petrecu/eseu-structurat-ultima-nopate-de-dragoste-intaia-nopate-de-razboi-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"Camil Petre",
         opera:"Tema si viziune-Ultima nopate de dragoste Intaia nopate de razboi",
-        href:""
+        href:"autori/eseuri/camil-petrecu/eseu-structurat-ultima-nopate-de-dragoste-intaia-nopate-de-razboi-tema-si-viziune.html"
     },
     {
         numeAutor:"Constantin Negrutii",
         opera:"Particularitatile personajului principal-Alexandru Lapusneanu",
-        href:""
+        href:"autori/eseuri/constantin-negrutzii/eseu-structurat-alexandru-lapusneanu-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"Constantin Negrutii",
         opera:"Relatia dintre 2 personaje-Alexandru Lapusneanu",
-        href:""
+        href:"autori/eseuri/constantin-negrutzii/eseu-structurat-alexandru-lapusneanu-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"Constantin Negrutii",
         opera:"Tema si viziune-Alexandru Lapusneanu",
-        href:""
+        href:"autori/eseuri/constantin-negrutzii/eseu-structurat-alexandru-lapusneanu-tema-si-viziune.html"
     },
     {
         numeAutor:"George Bacovia",
         opera:"Tema si viziune-Plumb",
-        href:""
+        href:"autori/eseuri/george-bacovia/eseu-structurat-plumb-tema-si-viziune.html"
     },
     {
         numeAutor:"George Calinescu",
         opera:"Particularitatile personajului principal-Enigma Otiliei",
-        href:""
+        href:"autori/eseuri/george-calinescu/eseu-structurat-enigma-otiliei-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"George Calinescu",
         opera:"Relatia dintre 2 personaje-Enigma Otiliei",
-        href:""
+        href:"autori/eseuri/george-calinescu/eseu-structurat-enigma-otiliei-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"George Calinescu",
         opera:"Tema si viziune-Enigma Otiliei",
-        href:""
+        href:"autori/eseuri/george-calinescu/eseu-structurat-enigma-otiliei-tema-si-viziune.html"
     },
     
     {
         numeAutor:"Ioan Slavici",
         opera:"Particularitatile personajului principal-Moara cu Noroc",
-        href:""
+        href:"autori/eseuri/ioan-slavici/eseu-structurat-moara-cu-noroc-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"Ioan Slavici",
         opera:"Relatia dintre 2 personaje-Moara cu Noroc",
-        href:""
+        href:"autori/eseuri/ioan-slavici/eseu-structurat-moara-cu-noroc-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"Ioan Slavici",
         opera:"Tema si viziune-Moara cu Noroc",
-        href:""
+        href:"autori/eseuri/ioan-slavici/eseu-structurat-moara-cu-noroc-tema-si-viziune.html"
     },
     
     {
         numeAutor:"Ion Barbu",
         opera:"Tema si viziune-Riga Crypto si Lapona Enigel",
-        href:""
+        href:"autori/eseuri/ion-barbu/eseu-structurat-riga-crypto-si-lapona-enigel-tema-si-viziune.html"
     },
     {
         numeAutor:"Ion Creanga",
         opera:"Particularitatile personajului principal-Harap Alb",
-        href:""
+        href:"autori/eseuri/ion-creanga/eseu-structurat-harap-alb-particularitatile-personajuliu-principal.html"
     },
     {
         numeAutor:"Ion Creanga",
         opera:"Relatia dintre 2 personaje-Harap Alb",
-        href:""
+        href:"autori/eseuri/ion-creanga/eseu-structurat-harap-alb-relatia-dintre-2-personaje.html"
     },
     {
         numeAutor:"Ion Creanga",
         opera:"Tema si viziune-Harap Alb",
-        href:""
+        href:"autori/eseuri/ion-creanga/eseu-structurat-harap-alb-tema-si-viziune.html"
     },
     {
         numeAutor:"Ion Luca Caragiale",
         opera:"Particularitatile personajului principal-O scrisoare pierduta",
-        href:""
+        href:"autori/eseuri/ion-luca-caragiale/eseu-structurat-o-scrisoare-pierduta-particularitatile-personajului-principal.html"
     },
     {
         numeAutor:"Ion Luca Caragiale",
         opera:"Relatia dintre 2 personaje-O scrisoare pierduta",
-        href:""
+        href:"autori/eseuri/ion-luca-caragiale/eseu-structurat-o-scrisoare-pierduta-relatia-dintre-2-personaje.html"
     },
     {
         numeAutor:"Ion Luca Caragiale",
         opera:"Tema si viziune-O scrisoare pierduta",
-        href:""
+        href:"autori/eseuri/ion-luca-caragiale/eseu-structurat-o-scrisoare-pierduta-tema-si-viziune.html"
     },
     {
         numeAutor:"Ion Pilat",
         opera:"Tema si viziune-Aci sosi pe vremuri",
-        href:""
+        href:"autori/eseuri/ion-pilat/eseu-structurat-aci-sosi-pe-vremuri-tema-si-viziune.html"
     },
     {
         numeAutor:"Liviu Rebreanu",
         opera:"Particularitatile personajuliu principal-Ion",
-        href:""
+        href:"autori/eseuri/liviu-rebreanu/eseu-structurat-ion-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"Liviu Rebreanu",
         opera:"Relatia dintre 2 personaje-Ion",
-        href:""
+        href:"autori/eseuri/liviu-rebreanu/eseu-structurat-ion-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"Liviu Rebreanu",
         opera:"Tema si viziune-Ion",
-        href:""
+        href:"autori/eseuri/liviu-rebreanu/eseu-structurat-ion-tema-si-viziune.html"
     },
     {
         numeAutor:"Lucian Blaga",
         opera:"Tema si viziune-Eu nu strivesc corola de minuni a lumii",
-        href:""
+        href:"autori/eseuri/lucian-blaga/eseu-structurat-eu-nu-strivesc-corola-de-minuni-a-lumii-tema-si-viziune.html"
     },
     {
         numeAutor:"Mihai Eminescu",
         opera:"Tema si viziune-Luceafar",
-        href:""
+        href:"autori/eseuri/mihai-eminescu/eseu-structurat-luceafar-tema-si-viziune.html"
     },
     {
         numeAutor:"Mihail Sadoveanu",
         opera:"Particularitatile personajului principal-Baltagul",
-        href:""
+        href:"autori/eseuri/mihail-sadoveanu/eseu-structurat-baltagul-particularitatile-personajului-principal.html"
     },
     
     {
         numeAutor:"Mihail Sadoveanu",
         opera:"Relatia dintre 2 personaje-Baltagul",
-        href:""
+        href:"autori/eseuri/mihail-sadoveanu/eseu-structurat-baltagul-relatia-dintre-2-personaje.html"
     },
     
     {
         numeAutor:"Mihail Sadoveanu",
         opera:"Tema si viziune-Baltagul",
-        href:""
+        href:"autori/eseuri/mihail-sadoveanu/eseu-structurat-baltagul-tema-si-viziune.html"
     },
     {
         numeAutor:"Mircea Eliade",
         opera:"Particularitatile personajuliu principa-Maitreyi",
-        href:""
+        href:"autori/eseuri/mircea-eliade/eseu-structurat-maitreyi-particularitatile-personajuliu-principal.html"
     },
     {
         numeAutor:"Mircea Eliade",
         opera:"Relatia dintre 2 personaje-Maitreyi",
-        href:""
+        href:"autori/eseuri/mircea-eliade/eseu-structurat-maitreyi-relatia-dintre-2-personaje.html"
     },
     {
         numeAutor:"Mircea Eliade",
         opera:"Tema si viziune-Maitreyi",
-        href:""
+        href:"autori/eseuri/mircea-eliade/eseu-structurat-maitreyi-tema-si-viziune.html"
     },
     
     {
         numeAutor:"Tudor Arghezi",
         opera:"Tema si viziune-Flori de Mucigai",
-        href:""
+        href:"autori/eseuri/tudor-arghezi/eseu-structurat-flori-de-mucigai-tema-si-viziune.html"
     },
     
 ];
